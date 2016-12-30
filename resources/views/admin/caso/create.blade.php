920b62ce5bd73c82f1f48bd1306c2979b6442931
nixon <nixontorres26@gmail.com>
2016-12-29 22:14:21 -0430

    caso create - bug fix

--- resources/views/admin/caso/create.blade.php
+++ resources/views/admin/caso/create.blade.php
@@ -20,7 +20,33 @@
             <div class="row panel panel-default">
                 {!! Form::open(['route' => 'admin.caso.store']) !!}
 
-                    @include('admin.caso.fields')
+            <!-- Evaluacion Field -->
+            <div class="form-group col-md-12">
+            {!! Form::label('evaluacion', 'Evaluacion:') !!}
+                <select name="evaluacion_id" class="form-control">
+                @foreach ($evaluacions as $evaluacion)
+                <option value="{{ $evaluacion->id }}">{{ $evaluacion->titulo }} 
+                </option>
+                @endforeach
+                </select>
+            </div>
+            <!-- titulo Field -->
+            <div class="form-group col-md-12">
+                {!! Form::label('titulo', 'Titulo:') !!}
+                {!! Form::text('titulo', null, ['class' => 'form-control','required']) !!}
+            </div>
+            <!-- enunciado Field -->
+            <div class="form-group col-md-12">
+                {!! Form::label('enunciado', 'Enunciado:') !!}
+                {!! Form::textarea('enunciado', null, ['class' => 'form-control','required']) !!}
+            </div>
+            <!-- Submit Field -->
+            <div class="form-group col-md-12">
+            <center>
+            {!! Form::submit('Registrar', ['class' => 'btn btn-default']) !!}
+            <a href="{!! route('admin.estudiante.index') !!}" class="btn btn-warning">Cancelar</a>
+            </center>
+            </div>
 
                 {!! Form::close() !!}
             </div>    
