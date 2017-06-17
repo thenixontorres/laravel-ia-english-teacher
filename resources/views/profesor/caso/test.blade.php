69f2340cad7754db5388f5815fb9fd8ce6818287
Nixon <nixontorres26@gmail.com>
2017-06-16 23:32:09 -0400

    bot modificado en porofesor - test

--- resources/views/profesor/caso/test.blade.php
+++ resources/views/profesor/caso/test.blade.php
@@ -7,7 +7,22 @@
                 <h4 class="title">{{ $caso->titulo }}</h4>
             </div>
             <div class="card-content table-responsive">        
-            	<div class="row">
+            	<!-- mensaje del usuario -->
+                @if(isset($respuesta))
+                <div class="row" id="user-messege">
+                    <div class="col-md-9">
+                           <div class="card"><strong>Tu dijiste:</strong> {{$mensaje}}</div>  
+                    </div>
+                    <div class="col-md-3">
+                            <img src="{{ asset(Auth::user()->persona->foto) }}" class="img img-responsive">
+                            <center>
+                            {{Auth::user()->persona->nombre}}
+                            </center>
+                    </div>
+                </div>
+                @endif
+                <!-- respuesta del bot -->
+                <div class="row" id="bot-messege">
             		<div class="col-md-3">
             			@if(isset($reaccion))
             				<img src="{{ asset($reaccion->reaccion)}}" class="img img-responsive">
@@ -23,8 +38,7 @@
             		</div>
             		<div class="col-md-9">
 		            	@if(isset($respuesta))
-		            		<div class="card">Tu dijiste: {{$mensaje}}</div>
-		            		<div class="card">Bot respondio: {{$respuesta->respuesta}}</div>
+		            		<div class="card"><strong>Tyler respondio: </strong>{{$respuesta->respuesta}}</div>
 		            	@else
 		            		<div class="card"><strong> Bot dice: </strong> Hola! que tal? necesito tu ayuda. </div>
 		            	@endif
